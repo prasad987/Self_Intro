cb938ac216604434bad08ca1c65de750e45d2651
prasad987 <12prasadmudhol@gmail.com>
2021-10-03 14:26:53 +0530

    Updated
    up

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Student", "Bug_BountyHunter", "Ethical_Hacker", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Student", "Bug_BountyHunter", "Ethical_Hacker", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
