d5534d3274ee10753f2918fd57176118d59056ee
Prasad Mudhole <12prasadmudhol@gmail.com>
2022-09-09 13:05:02 +0530

    Update2022
    Re-New

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Bug_BountyHunter", "Ethical_Hacker", "Designer", "Freelancer"],
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student", "Bug_BountyHunter", "Ethical_Hacker", "Designer", "Freelancer"],
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
